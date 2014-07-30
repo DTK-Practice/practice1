88527182527e24cb1bba88bc69a6718150bb62c0
Dustin Kocher <dtkocher@gmail.com>
2014-07-30 14:00:17 -0400

    Add the ability to add a organization_role to user.

--- exercise_2/app/model/user.rb
+++ exercise_2/app/model/user.rb
@@ -1,7 +1,20 @@
 class User
-  attr_accessor :name
+  attr_accessor :name, :organization_roles
 
   def initialize(args={})
     @name = args.fetch(:name, nil)
+    @organization_roles = args.fetch(:organization_roles, [])
+  end
+
+  def add_organization_role(organization_role)
+    raise "This Organization Role already is added" if organization_role_already_exists(organization_role)
+    @organization_roles << organization_role unless organization_role.nil?
+  end
+
+  private
+
+  def organization_role_already_exists(organization_role)
+    return false if organization_roles.empty?
+    organization_roles.each{|org_role| org_role == organization_role }
   end
 end
--- exercise_2/spec/model/user_spec.rb
+++ exercise_2/spec/model/user_spec.rb
@@ -7,5 +7,29 @@ describe User do
     let(:user) { User.new({name: "bob"}) }
 
     its(:name) { should eq("bob") }
+    its(:organization_roles) { should eq([]) }
+  end
+
+  describe "#add_organization_role" do
+    let(:user) { User.new }
+    before :each do
+      @org1 = Organization.new({name: "First"})
+
+      @org_role1 = OrganizationRole.new({organization: @org1, role: Role.admin})
+      @org_role2 = OrganizationRole.new({organization: @org1, role: Role.admin})
+      user.add_organization_role(@org_role1)
+    end
+
+    context "add a valid organization_role" do
+      it "organization_roles should equal @org_role1" do
+        user.organization_roles.should eq([@org_role1])
+      end
+    end
+
+    context "try to add a invalid organization_role" do
+      it "should raise error that Organization has already been added" do
+        expect {user.add_organization_role(@org_role2)}.to raise_error
+      end
+    end
   end
 end
