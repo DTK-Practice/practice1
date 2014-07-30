c56ad01275327073b15cdefbfa508988e3c4e894
Dustin Kocher <dtkocher@gmail.com>
2014-07-30 10:56:22 -0400

    Fix UserOrganizationRole test.

--- exercise_2/spec/model/user_spec.rb
+++ exercise_2/spec/model/user_spec.rb
@@ -2,39 +2,10 @@ require_relative '../spec_helper.rb'
 
 describe User do
   subject { user }
-  describe "#admin?" do
-    context "is admin" do
-      let(:user) { User.new({role: Role.admin}) }
-      its(:admin?) { should be true }
-    end
 
-    context "not admin" do
-      let(:user) { User.new({role: Role.denied}) }
-      its(:admin?) { should be false }
-    end
-  end
+  describe "#new" do
+    let(:user) { User.new({name: "bob"}) }
 
-  describe "#user?" do
-    context "is user" do
-      let(:user) { User.new({role: Role.user}) }
-      its(:user?) { should be true }
-    end
-
-    context "not user" do
-      let(:user) { User.new({role: Role.denied}) }
-      its(:user?) { should be false }
-    end
-  end
-
-  describe "#denied?" do
-    context "is denied" do
-      let(:user) { User.new({role: Role.denied}) }
-      its(:denied?) { should be true }
-    end
-
-    context "not denied" do
-      let(:user) { User.new({role: Role.admin}) }
-      its(:denied?) { should be false }
-    end
+    its(:name) { should eq("bob") }
   end
 end
