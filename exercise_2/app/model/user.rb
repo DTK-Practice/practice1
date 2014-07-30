57ff7d5745c932b4f37f9b23d301b865b0231c5e
Dustin Kocher <dtkocher@gmail.com>
2014-07-30 10:58:48 -0400

    Create user class.

--- exercise_2/app/model/user.rb
+++ exercise_2/app/model/user.rb
@@ -1,21 +1,7 @@
 class User
-  attr_accessor :role, :name, :organization
+  attr_accessor :name
 
   def initialize(args={})
-    @role = args.fetch(:role, Role.denied)
     @name = args.fetch(:name, nil)
-    @organization = args.fetch(:organization, nil)
-  end
-
-  def admin?
-    role == Role.admin
-  end
-
-  def user?
-    role == Role.user
-  end
-
-  def denied?
-    role == Role.denied
   end
 end
